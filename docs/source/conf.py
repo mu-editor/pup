58673257cec896486d90a5c9dede32641ca9835d
Tiago Montes <tiago.montes@gmail.com>
2020-08-12 19:17:42 +0100

    Adjust Sphinx config to guide readthedocs.org in building the docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,3 +57,11 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- readthedocs.org  --------------------------------------------------------
+
+# The master toctree document.
+# Motive: https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
+

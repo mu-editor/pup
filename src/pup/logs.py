6e844f65caf9d3e0f9615ead48213a5255f65f05
Tiago Montes <tiago.montes@gmail.com>
2020-10-13 16:05:02 +0100

    Simpler log format when output is a TTY.

--- src/pup/logs.py
+++ src/pup/logs.py
@@ -2,7 +2,9 @@
 PUP logging setup module.
 """
 
+import os
 import logging
+import sys
 
 
 
@@ -12,13 +14,20 @@ def _level_from_string(log_level_str):
 
 
 
+_OUTPUT_TO_TTY_FORMAT = {
+    False: '%(asctime)s %(levelname).1s %(name)s %(message)s',
+    True: '%(levelname).1s %(message)s',
+}
+
 def start(log_level_str):
     """
     Starts logging messages of severity `log_level_str` or higher to STDERR.
+    Logged messages include more detail when STDERR is not a TTY.
     """
+    output_is_tty = os.isatty(sys.stderr.fileno())
     logging.basicConfig(
         level=_level_from_string(log_level_str),
-        format='%(asctime)s %(levelname).1s %(name)s %(message)s',
+        format=_OUTPUT_TO_TTY_FORMAT[output_is_tty],
         datefmt='%Y%m%d %H%M%S',
     )
     logging.captureWarnings(True)

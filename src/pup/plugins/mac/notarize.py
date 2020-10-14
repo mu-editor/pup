93ead94a159558dcf458018ea5c4bbcf33513af3
Tiago Montes <tiago.montes@gmail.com>
2020-10-14 20:28:38 +0100

    Notarization request now being successfully tracked.

--- src/pup/plugins/mac/notarize.py
+++ src/pup/plugins/mac/notarize.py
@@ -5,14 +5,8 @@ PUP Plugin implementing the 'mac.notarize-app-bundle' step.
 import logging
 import os
 import subprocess
-
-try:
-    # Python < 3.9
-    import importlib_resources as ilr
-except ImportError:
-    import importlib.resources as ilr
-
-from . import sign_resources
+import time
+import xml.etree.ElementTree as et
 
 
 
@@ -45,8 +39,8 @@ class Step:
 
 
         app_bundle_zip = self._create_app_bundle_zip(ctx, dsp)
-        request_id = self._request_notarization(ctx, dsp, app_bundle_zip, user, password)
-        self._wait_notarization(dsp, request_id)
+        request_uuid = self._request_notarization(ctx, dsp, app_bundle_zip, user, password)
+        self._wait_notarization(dsp, request_uuid, user, password)
         self._staple_app_bundle(dsp)
 
 
@@ -82,12 +76,39 @@ class Step:
         return build_dir / app_bundle_zip
 
 
+    def _parse_xml_plist(self, xml_payload):
+        """
+        Returns a `dict` built from parsing the XML Property List in `xml_payload`.
+        """
+        root_element = et.fromstring(xml_payload)
+        xml_dict, *tail = root_element.findall('./dict')
+        if len(tail):
+            _log.warning('Multiple dicts in XML plist: %r', xml_payload)
+        return self._parse_xml_dict(xml_dict)
+
+
+    def _parse_xml_dict(self, element):
+        """
+        Returns a `dict` built from recursively parsing an XML element tree
+        starting with a <dict> element, followed by key/value element pairs:
+        keys are the text within <key> elements, values are the text within
+        the following elements (if such elements are <dict>, the function is
+        called recursively).
+        """
+        result = {}
+        element_iter = iter(element)
+        for k_element in element_iter:
+            v_element = next(element_iter)
+            k = k_element.text
+            v = self._parse_xml_dict(v_element) if v_element.tag == 'dict' else v_element.text
+            result[k] = v
+        return result
+
+
     def _request_notarization(self, ctx, dsp, app_bundle_zip, user, password):
 
-        xcrun = self._cli_command_path('xcrun')
-
         cmd = [
-            xcrun,
+            self._cli_command_path('xcrun'),
             'altool',
             '--notarize-app',
             '--primary-bundle-id',
@@ -101,17 +122,81 @@ class Step:
             '--file',
             app_bundle_zip,
         ]
+        xml_output_lines = []
         _log.info('Requesting notarization...')
         dsp.spawn(
             cmd,
-            out_callable=lambda line: _log.info('altool| %s', line),
+            out_callable=xml_output_lines.append,
             err_callable=lambda line: _log.info('altool! %s', line),
         )
+        xml_payload = '\n'.join(xml_output_lines)
+        _log.debug('xml_payload=%r', xml_payload)
+        response = self._parse_xml_plist(xml_payload)
+        try:
+            request_uuid = response['notarization-upload']['RequestUUID']
+        except KeyError as exc:
+            _log.critical('Missing %s key in notarization request output.', str(exc))
+            request_uuid = None
+
+        _log.info('Notarization request submitted: RequestUUID=%r.', request_uuid)
+        return request_uuid
 
 
-    def _wait_notarization(self, dsp, request_id):
+    def _wait_notarization(self, dsp, request_uuid, user, password, delay=60, tolerance=600):
 
-        _log.info('TODO: wait notarization')
+        cmd = [
+            self._cli_command_path('xcrun'),
+            'altool',
+            '--notarization-info',
+            request_uuid,
+            '--username',
+            user,
+            '--password',
+            password,
+            '--output-format',
+            'xml',
+        ]
+        start = time.time()
+        while True:
+            _log.info('Requesting notarization info in %ss...', delay)
+            time.sleep(delay)
+            xml_output_lines = []
+            dsp.spawn(
+                cmd,
+                out_callable=xml_output_lines.append,
+                err_callable=lambda line: _log.info('altool! %s', line),
+            )
+            xml_payload = '\n'.join(xml_output_lines)
+            _log.debug('xml_payload=%r', xml_payload)
+            response = self._parse_xml_plist(xml_payload)
+            response_info = response['notarization-info'] 
+            try:
+                status = response_info['Status']
+            except KeyError as exc:
+                _log.warning('Missing %s key in notarization info output.', str(exc))
+            else:
+                _log.info('Status is %r.', status)
+            if status != 'in progress':
+                break
+            if time.time() - start > tolerance:
+                _log.warning('Slow notarization: see https://developer.apple.com/system-status/')
+
+        status_code = response_info['Status Code']
+        status_msg = response_info['Status Message']
+        log_url = response_info['LogFileURL']
+        if status != 'success':
+            _log.error(
+                'Notarization failed: %r. Check log at %r.',
+                status_msg,
+                log_url,
+            )
+        elif status_code != '0':
+            _log.warning(
+                'Unexpected notarization status code %r: %r. Check log at %r.',
+                status_code,
+                status_msg,
+                log_url,
+            )
 
 
     def _staple_app_bundle(self, dsp):

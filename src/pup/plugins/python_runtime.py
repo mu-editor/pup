6593276c55135303f97119b62dac37e873af1f28
Carlos Pereira Atencio <carlosperate@users.noreply.github.com>
2021-08-27 14:36:38 +0100

    Update Python Standalone URLs to latest 3.8 release

--- src/pup/plugins/python_runtime.py
+++ src/pup/plugins/python_runtime.py
@@ -20,11 +20,11 @@ _log = logging.getLogger(__name__)
 _PYTHON_BUILD_STANDALONE_URLs = {
     'darwin': {
         '3.7': 'https://github.com/indygreg/python-build-standalone/releases/download/20200823/cpython-3.7.9-x86_64-apple-darwin-pgo-20200823T2228.tar.zst',
-        '3.8': 'https://github.com/indygreg/python-build-standalone/releases/download/20200823/cpython-3.8.5-x86_64-apple-darwin-pgo-20200823T2228.tar.zst',
+        '3.8': 'https://github.com/indygreg/python-build-standalone/releases/download/20210724/cpython-3.8.11-x86_64-apple-darwin-pgo-20210724T1424.tar.zst',
     },
     'win32': {
         '3.7': 'https://github.com/indygreg/python-build-standalone/releases/download/20200822/cpython-3.7.9-x86_64-pc-windows-msvc-shared-pgo-20200823T0118.tar.zst',
-        '3.8': 'https://github.com/indygreg/python-build-standalone/releases/download/20200830/cpython-3.8.5-x86_64-pc-windows-msvc-shared-pgo-20200830T2254.tar.zst',
+        '3.8': 'https://github.com/indygreg/python-build-standalone/releases/download/20210724/cpython-3.8.11-x86_64-pc-windows-msvc-shared-pgo-20210724T1424.tar.zst',
     },
 }
 

c6ac673b88229ed38f6dfba791ae050825424169
Tiago Montes <tiago.montes@gmail.com>
2021-10-31 16:27:40 +0000

    Preliminary work towards multi-format macOS packaging.

--- src/pup/plugins/mac/steps.py
+++ src/pup/plugins/mac/steps.py
@@ -3,7 +3,9 @@ PUP Plugin defining macOS packaging stages.
 """
 
 
-class Steps:
+class DMG:
+
+    FORMAT = 'dmg'
 
     @staticmethod
     def usable_in(ctx):
@@ -22,4 +24,29 @@ class Steps:
             'mac.sign-app-bundle',
             'mac.notarize-app-bundle',
             'mac.create-dmg',
+        )
+
+
+
+class ZIP:
+
+    FORMAT = 'zip'
+
+    @staticmethod
+    def usable_in(ctx):
+        return (
+            (ctx.pkg_platform == 'darwin') and
+            (ctx.tgt_platform == 'darwin')
+        )
+
+    def __call__(self, ctx, _dsp):
+        return (
+            'pup.python-runtime',
+            'mac.app-bundle-template',
+            'pup.pip-install',
+            'pup.install-cleanup',
+            'mac.launcher',
+            'mac.sign-app-bundle',
+            'mac.notarize-app-bundle',
+            'mac.create-zip',
         )

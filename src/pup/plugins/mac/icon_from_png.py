793e4af2d62263c78b81c01bca2fc420799b9958
Tiago Montes <tiago.montes@gmail.com>
2022-06-18 22:23:28 +0100

    Don't fail when no --icon-path is given!

--- src/pup/plugins/mac/icon_from_png.py
+++ src/pup/plugins/mac/icon_from_png.py
@@ -34,7 +34,7 @@ class Step:
 
     def __call__(self, ctx, dsp):
 
-        if ctx.icon_path.suffix.upper() != '.PNG':
+        if not ctx.icon_path or ctx.icon_path.suffix.upper() != '.PNG':
             return
 
         build_dir = dsp.directories()['build']
